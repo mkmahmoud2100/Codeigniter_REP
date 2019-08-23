5b2aca4309246c18d8c0b04236da016f89a06f97
mkmahmoud2100 <mkmahmoud2100@users.noreply.github.com>
2019-08-23 22:10:14 +0300

    Add files via upload

---
+++ application/language/information_lang.php
@@ -0,0 +1,172 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/*object */
+
+$lang['Width']='';
+$lang['Height']='';
+$lang['Weight']='';
+$lang['Valume']='';
+$lang['Area']='';
+$lang['Size']='';
+
+/* person */
+$lang['name'] = 'Ad';
+$lang['Surnam'] = 'Soyad';
+$lang['Fullname'] = 'Ad Soyad';
+$lang['Country'] = 'Uyruk';
+$lang['Post'] = 'Posta';
+$lang['Email'] = 'Posta';
+$lang['Address'] = 'Adres';
+$lang['Mobile'] = 'Cep';
+$lang['Phone'] = 'Tel';
+$lang['Home'] = 'Ev';
+$lang['Street'] = 'Cedda';
+$lang['Member']='';
+$lang['Merital Status']='';
+$lang['Country']='';
+
+/* date time */
+$lang['Second'] = 'Saniya';
+$lang['Minute'] = 'Dakka';
+$lang['Houre'] = 'Saat';
+$lang['Day'] = 'Gün';
+$lang['Month'] = 'Ay';
+$lang['Year'] = 'Yil';
+$lang['Date'] = 'Terih';
+$lang['Time'] = 'Zaman';
+$lang['Start Date'] = 'Başlangıç Tarih';
+$lang['Last Date'] = 'Son Tarih';
+$lang['Duration In Days'] = 'Gün Sayısı';
+$lang['Today'] = 'Bugün';
+$lang['Start On Time'] = 'Zamanında Başladı';
+$lang['Lateness'] = 'Gecikme';
+$lang['In time'] = 'Zamanında';
+$lang['Shift To'] = 'Kayma';
+
+/* fees */
+$lang['Unit Price'] = 'Birim Fiyeti';
+$lang['Total Price'] = 'Toplam';
+$lang['General Total'] = 'Genel Toplam';
+$lang['Price'] = 'Fiyet';
+$lang['Amount'] = 'Miktar';
+$lang['VAT'] = 'KDV';
+$lang['Discount']='İndirim';
+$lang['Offer']='Kampanya';
+$lang['Extras'] = 'Ekstralar';
+$lang['Min Price'] = 'Min fiyat';
+$lang['Max Price'] = 'Max fiyat';
+
+/* Currencies*/
+
+/* position */
+$lang['Left'] = 'Sol';
+$lang['Right'] = 'Sağ';
+$lang['Under']='Alt';
+$lang['Above']='Ust';
+$lang['in']='İç';
+$lang['out']='Diş';
+$lang['behind']='Arka';
+
+/* buttons */
+$lang['Watch'] = 'Izle';
+$lang['Follow'] = 'Tekib et';
+$lang['Next'] = 'Sonra';
+$lang['End'] = 'Son';
+$lang['back'] = 'Once';
+$lang['Save'] = 'Kaydet';
+$lang['Delete'] = 'Sil';
+$lang['Edit'] = 'Düzenle';
+$lang['New'] = 'Yeni';
+$lang['Cancel'] = 'İptal';
+$lang['Undo'] = 'Geri';
+$lang['Redo'] = 'Bitir';
+$lang['Calculate'] = 'Hesapla';
+$lang['Close'] = 'Kapat';
+$lang['Logout']='Çikis yap';
+$lang['Login']='Giriş yap';
+$lang['Signin']='Üye ol';
+$lang['Signout']='';
+$lang['chat']='';
+
+/* menu */
+
+$lang['Home Page'] = 'Ana Sayfa';
+$lang['About Us'] = 'Hakkımızda';
+$lang['Contact Us'] = 'Bize Ulaşın';
+$lang['Schedule'] = 'Program';
+$lang['Laptop'] = 'Diz Ustu';
+$lang['Mobile'] = 'Cep';
+$lang['Other'] = 'Diger';
+$lang['Error'] = 'Hata';
+$lang['Video']='Video';
+$lang['Image']='Rasim';
+
+/* Media */
+$lang['Format']='biçim';
+$lang['Size On Disk']='';
+$lang['Gigabyte']='';
+$lang['Megabyte']='';
+$lang['Kilobyte']='';
+$lang['Byte']='';
+$lang['Bit']='';
+$lang['Memory']='';
+$lang['Hard Disk']='sabit disk';
+$lang['High Definition']='';
+
+/* group */
+$lang['Group']='';
+$lang['Member']='';
+$lang['Join']='';
+$lang['Ungroup']='';
+$lang['Create Group']='';
+$lang['Update Group']='';
+$lang['Edit Group']='';
+$lang['Dissmis Group']='';
+$lang['Stop Group']='';
+
+/* */
+
+$lang['Topic']='Konu';
+$lang['Subject']='kırılım';
+
+$lang['Team'] = 'Takım';
+$lang['Encounter'] = 'Karşılaşma';
+$lang['Place'] = 'Yar';
+
+/* places */
+/* arrangment */
+$lang['Queue']='';
+$lang['List']='';
+$lang['Buffer']='';
+$lang['First']='';
+$lang['Second']='';
+$lang['Last']='';
+$lang['Ordered']='';
+$lang['Outorder']='';
+$lang['Serial']='';
+$lang['Parallel']='';
+
+/* motion*/
+/**/
+/* general */
+$lang['Advertisment'] = 'Reklam';
+$lang['Advertisment Id'] = 'Reklam Kimliği';
+$lang['Advertisment Group'] = 'Reklam Grubu';
+$lang['Title'] = 'Başlık';
+$lang['Advertise Here'] = 'Burada Reklam Ver';
+$lang['Customer Id'] = 'Müşteri Kimliği';
+$lang['Customer Group'] = 'Müşteri Grubu';
+$lang['Image Link'] = 'Resim Bağlantısı';
+$lang['Contents'] = 'İçerik';
+$lang['Icon'] = 'Simga';
+$lang['Message'] = 'Mesaj';
+$lang['Position'] = 'Pozisyon';
+
+
+$lang['index'] = 'Indeks';
---
+++ application/language/medical_lang.php
@@ -0,0 +1,132 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+$lang['Team'] = 'Takım';
+$lang['Encounter'] = 'Karşılaşma';
+$lang['Place'] = 'Yar';
+
+/* buttons */
+$lang['Watch'] = 'Izle';
+$lang['Follow'] = 'Tekib et';
+$lang['Next'] = 'Sonra';
+$lang['End'] = 'Son';
+$lang['back'] = 'Once';
+$lang['Save'] = 'Kaydet';
+$lang['Delete'] = 'Sil';
+$lang['Edit'] = 'Düzenle';
+$lang['New'] = 'Yeni';
+$lang['Cancel'] = 'İptal';
+$lang['Undo'] = 'Geri';
+$lang['Redo'] = 'Bitir';
+$lang['Calculate'] = 'Hesapla';
+$lang['Close'] = 'Kapat';
+
+/* menu */
+
+$lang['Home Page'] = 'Ana Sayfa';
+$lang['About Us'] = 'Hakkımızda';
+$lang['Contact Us'] = 'Bize Ulaşın';
+$lang['Schedule'] = 'Program';
+$lang['Laptop'] = 'Diz Ustu';
+$lang['Mobile'] = 'Cep';
+$lang['Other'] = 'Diger';
+$lang['Error'] = 'Hata';
+
+/* person */
+
+$lang['name'] = 'Ad';
+$lang['Surnam'] = 'Soyad';
+$lang['Fullname'] = 'Ad Soyad';
+$lang['Country'] = 'Uyruk';
+$lang['Post'] = 'Posta';
+$lang['Email'] = 'Posta';
+$lang['Address'] = 'Adres';
+$lang['Mobile'] = 'Cep';
+$lang['Phone'] = 'Tel';
+$lang['Home'] = 'Ev';
+$lang['Street'] = 'Cedda';
+
+
+/* time */
+$lang['Second'] = 'Saniya';
+$lang['Minute'] = 'Dakka';
+$lang['Houre'] = 'Saat';
+$lang['Day'] = 'Gün';
+$lang['Month'] = 'Ay';
+$lang['Year'] = 'Yil';
+$lang['Date'] = 'Terih';
+$lang['Time'] = 'Zaman';
+$lang['Start Date'] = 'Başlangıç Tarih';
+$lang['Last Date'] = 'Son Tarih';
+$lang['Duration In Days'] = 'Gün Sayısı';
+$lang['Today'] = 'Bugün';
+$lang['Start On Time'] = 'Zamanında Başladı';
+$lang['Lateness'] = 'Gecikme';
+$lang['In time'] = 'Zamanında';
+$lang['Shift To'] = 'Kayma';
+
+/* fees */
+$lang['Unit Price'] = 'Birim Fiyeti';
+$lang['Total Price'] = 'Toplam';
+$lang['General Total'] = 'Genel Toplam';
+$lang['Price'] = 'Fiyet';
+$lang['Amount'] = 'Miktar';
+$lang['VAT'] = 'KDV';
+$lang['Extras'] = 'Ekstralar';
+$lang['Min Price'] = 'Min fiyat';
+$lang['Max Price'] = 'Max fiyat';
+
+/* general */
+$lang['Advertisment'] = 'Reklam';
+$lang['Advertisment Id'] = 'Reklam Kimliği';
+$lang['Advertisment Group'] = 'Reklam Grubu';
+$lang['Title'] = 'Başlık';
+$lang['Advertise Here'] = 'Burada Reklam Ver';
+$lang['Customer Id'] = 'Müşteri Kimliği';
+$lang['Customer Group'] = 'Müşteri Grubu';
+$lang['Image Link'] = 'Resim Bağlantısı';
+$lang['Contents'] = 'İçerik';
+$lang['Icon'] = 'Simga';
+$lang['Message'] = 'Mesaj';
+$lang['Position'] = 'Pozisyon';
+$lang['Left'] = 'Sol';
+$lang['Right'] = 'Sağ';
+$lang['index'] = 'Indeks';
+
+/** computer related * */
+
+$lang['Producer'] = 'Üretici';
+$lang['Model'] = 'model';
+$lang['Color'] = 'renk';
+$lang['Operating_system'] = 'işletim sistemi';
+$lang['Processor'] = 'işlemci';
+$lang['Available_processors'] = 'Kullanılabilir İşlemciler';
+$lang['Chipset'] = 'Yonga seti';
+$lang['Memory'] = 'Bellek';
+$lang['Storage_available'] = 'Kullanılabilir depo';
+$lang['Display'] = 'Görüntüle';
+$lang['Graphics'] = 'Grafik';
+$lang['Audio'] = 'Ses';
+$lang['Wireless'] = 'Kablosuz';
+$lang['Expansion_slot'] = 'Genişleme Yuvası';
+$lang['Ports'] = 'Bağlantı noktaları';
+$lang['Camera'] = 'Kamera';
+$lang['Dimensions'] = 'Boyutlar';
+$lang['Weight'] = 'Ağırlık';
+$lang['Power'] = 'Güç';
+$lang['Duration'] = 'Süre';
+$lang['Manufacturer'] = 'Üretici';
+$lang['Category'] = 'Kategorisi';
+$lang['Status'] = 'Durum';
+$lang['Quantity'] = 'Miktar';
+$lang['Real_quantity'] = 'Gerçek miktar';
+$lang['Sold_quantity'] = 'Satılan miktar';
+$lang['Price'] = 'Fiyat';
+$lang['Currency_code'] = 'Döviz cinsi';
+$lang['Image_link'] = 'Resim bağlantısı';
+$lang['Comments'] = 'yorumlar';
